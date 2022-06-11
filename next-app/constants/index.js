2d02743d2a25a223e988467c64008554ad837a1f
Sumit Kumar Gupta <SumitDev.Tech@gmail.com>
2022-06-11 13:24:21 +0530

    first commit

---
+++ next-app/constants/index.js
@@ -0,0 +1,69 @@
+export const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint8",
+        "name": "_maxWhitelistedAddresses",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "addAddressToWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "maxWhitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "numAddressesWhitelisted",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "whitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+// above `abi` taken from `hardhat-tutorial/artifacts/contracts/Whitelist.sol` folder 
+// and from the `Whitelist.json` file get the array marked under the "abi" key (it will be. a huge array, close to 100 lines if not more).
+
+export const WHITELIST_CONTRACT_ADDRESS = "0x721DC2e6D44e9cEEf64941DC35bc753451002d11"; // "YOUR_WHITELIST_CONTRACT_ADDRESS"
